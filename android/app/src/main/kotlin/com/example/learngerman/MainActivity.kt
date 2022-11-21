c9a5fef40e6911f56a387484a9f5256fc3116c38
= <=>
2022-11-21 15:46:15 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/learngerman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learngerman
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/schulzkilian/MeinZeug/Programme/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/schulzkilian/MeinZeug/Code/flutter/learngerman" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/schulzkilian/MeinZeug/Programme/flutter"
+  "PROJECT_DIR=/home/schulzkilian/MeinZeug/Code/flutter/learngerman"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/schulzkilian/MeinZeug/Code/flutter/learngerman/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/schulzkilian/MeinZeug/Code/flutter/learngerman/lib/main.dart"
+)
